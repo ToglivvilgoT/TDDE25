35c33d0f12840f2545157f6961123484d68d36a2
ToglivvilgoT <vilgot.johansen@hotmail.com>
2025-10-30 11:34:26 +0100

    ai turning and driving

--- ai.py
+++ ai.py
@@ -5,15 +5,17 @@ import math
 from collections import defaultdict, deque
 
 import pymunk
-from pymunk import Vec2d
+from pymunk import Vec2d, Space
 import gameobjects
+from gameobjects import Tank, GameObject
+from maps import Map
 
 # NOTE: use only 'map0' during development!
 
 MIN_ANGLE_DIF = math.radians(3)   # 3 degrees, a bit more than we can turn each tick
 
 
-def angle_between_vectors(vec1, vec2):
+def angle_between_vectors(vec1: Vec2d, vec2: Vec2d):
     """ Since Vec2d operates in a cartesian coordinate space we have to
         convert the resulting vector to get the correct angle for our space.
     """
@@ -33,15 +35,15 @@ class Ai:
     a breadth first search. Also capable of shooting other tanks and or wooden
     boxes. """
 
-    def __init__(self, tank, game_objects_list, tanks_list, space, currentmap):
+    def __init__(self, tank: Tank, game_objects_list: list[GameObject], tanks_list: list[Tank], space: Space, current_map: Map):
         self.tank = tank
         self.game_objects_list = game_objects_list
         self.tanks_list = tanks_list
         self.space = space
-        self.currentmap = currentmap
+        self.current_map = current_map
         self.flag = None
-        self.max_x = currentmap.width - 1
-        self.max_y = currentmap.height - 1
+        self.max_x = current_map.width - 1
+        self.max_y = current_map.height - 1
 
         self.path = deque()
         self.move_cycle = self.move_cycle_gen()
@@ -54,7 +56,7 @@ class Ai:
     def decide(self):
         """ Main decision function that gets called on every tick of the game.
         """
-        pass  # To be implemented
+        next(self.move_cycle)
 
     def maybe_shoot(self):
         """ Makes a raycast query in front of the tank. If another tank
@@ -67,15 +69,56 @@ class Ai:
             to move to our goal.
         """
         while True:
+            # find path
+            path = self.find_shortest_path()
+            if not path:
+                yield
+                continue
+            next_coord = path.popleft()
             yield
+
+            # turning
+            target_vector = Vec2d(*next_coord) - self.grid_pos
+            current_angle = target_vector.get_angle_between(self.tank.body.rotation_vector.rotated_degrees(90))
+            if current_angle > MIN_ANGLE_DIF:
+                self.tank.turn_left()
+            elif current_angle < -MIN_ANGLE_DIF:
+                self.tank.turn_right()
+            while abs(current_angle) > MIN_ANGLE_DIF:
+                yield
+                current_angle = target_vector.get_angle_between(self.tank.body.rotation_vector.rotated_degrees(90))
+            self.tank.stop_turning()
+
+            # driving
+            self.tank.accelerate()
+            target_center = next_coord + (0.5, 0.5)
+            distance = target_center.get_distance(self.tank.body.position)
+            prev_distance = distance + 1
+            while distance < prev_distance:
+                yield
+                prev_distance = distance
+                distance = target_center.get_distance(self.tank.body.position)
+            self.tank.stop_moving()
+            self.update_grid_pos()
 
     def find_shortest_path(self):
         """ A simple Breadth First Search using integer coordinates as our nodes.
             Edges are calculated as we go, using an external function.
         """
-        # To be implemented
-        shortest_path = []
-        return deque(shortest_path)
+        paths: dict[Vec2d, list[Vec2d]] = {self.grid_pos: []}
+        que = deque((self.grid_pos,))
+        visited = {self.grid_pos}
+        target = self.get_target_tile()
+        while que:
+            current = que.popleft()
+            if current == target:
+                return deque(paths[current])
+            for neighbor in self.get_tile_neighbors(current):
+                if neighbor not in visited:
+                    que.append(neighbor)
+                    visited.add(neighbor)
+                    paths[neighbor] = paths[current] + [neighbor]
+        return deque()
 
     def get_target_tile(self):
         """ Returns position of the flag if we don't have it. If we do have the flag,
@@ -105,15 +148,25 @@ class Ai:
         x, y = position_vector
         return Vec2d(int(x), int(y))
 
-    def get_tile_neighbors(self, coord_vec):
+    def get_tile_neighbors(self, coord_vec: Vec2d):
         """ Returns all bordering grid squares of the input coordinate.
             A bordering square is only considered accessible if it is grass
             or a wooden box.
         """
-        neighbors = []  # Find the coordinates of the tiles' four neighbors
+        neighbors = (coord_vec + offset for offset in ((1, 0), (0, 1), (-1, 0), (0, -1)))  # Find the coordinates of the tiles' four neighbors
         return filter(self.filter_tile_neighbors, neighbors)
+
+    def in_bounds(self, coord: Vec2d):
+        in_horizontal = 0 <= coord.x <= self.max_x
+        in_vertical = 0 <= coord.y <= self.max_y
+        return in_horizontal and in_vertical
 
     def filter_tile_neighbors(self, coord):
         """ Used to filter the tile to check if it is a neighbor of the tank.
         """
-        return True
+        if not self.in_bounds(coord):
+            return False
+
+        box_type = self.current_map.boxAt(*coord)
+        return box_type == 0 or box_type == 2
+
